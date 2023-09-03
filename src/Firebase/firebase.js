6ee1a13a770aa0613e324989c157c6844e4c2782
Khunshan07 <khunshan.intern@devsinc.com>
2023-09-04 00:04:25 +0500

    removed some build error

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -3,20 +3,10 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import.meta.env
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: import.meta.env.VITE_FIREBASE_APP_ID
-// };
 const firebaseConfig = {
   apiKey: "AIzaSyAh1zEvU1yVsNoR4yxd1CLaE1n-pRkv5N8",
   authDomain: "billbuddy-1a23f.firebaseapp.com",
