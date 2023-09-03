9814ad607bfc78f671fdcda28d82a094162bf506
Khunshan07 <khunshan.intern@devsinc.com>
2023-09-03 23:57:52 +0500

    Changed ENV File

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -9,13 +9,21 @@ import.meta.env
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_FIREBASE_APP_ID
+// };
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyAh1zEvU1yVsNoR4yxd1CLaE1n-pRkv5N8",
+  authDomain: "billbuddy-1a23f.firebaseapp.com",
+  projectId: "billbuddy-1a23f",
+  storageBucket: "billbuddy-1a23f.appspot.com",
+  messagingSenderId: "659029292057",
+  appId: "1:659029292057:web:b59e96ff8d7cebc5716688"
 };
 
 // Initialize Firebase
